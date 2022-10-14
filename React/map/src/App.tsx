ae9325f962c635228529fffe4e66a564f157f0dd
renata <renata.melo89@outlook.com>
2022-10-14 17:52:55 -0300

    atualizando pasta Map

---
+++ React/map/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import MapPage from "./pages/MapPage";
+
+export const REACT_APP_GOOGLE_API_KEY = "CHAVE-API";
+
+function App() {
+  return <MapPage />;
+}
+
+export default App;
